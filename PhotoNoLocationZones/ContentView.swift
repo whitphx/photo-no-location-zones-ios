85f08930f53aabaad77a02421c8318588205ea65
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2026-05-07 10:43:59 +0900

    Initial Commit

---
+++ PhotoNoLocationZones/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoNoLocationZones
+//
+//  Created by Yuichiro Tachibana on 2026/05/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoNoLocationZones/PhotoNoLocationZonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoNoLocationZonesApp.swift
+//  PhotoNoLocationZones
+//
+//  Created by Yuichiro Tachibana on 2026/05/07.
+//
+
+import SwiftUI
+
+@main
+struct PhotoNoLocationZonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
